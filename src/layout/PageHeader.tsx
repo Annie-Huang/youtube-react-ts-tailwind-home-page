9a9c1b3f2d0bc385e523e65262b44a0675999494
Annie Huang <annie.hn@gmail.com>
2023-10-21 17:31:29 +1100

    Add the first column of the <PageHeader> section

--- src/layout/PageHeader.tsx
+++ src/layout/PageHeader.tsx
@@ -1,3 +1,15 @@
+import logo from '../assets/Logo.png';
 export const PageHeader = () => {
-  return <div></div>;
+  return (
+    <div className='flex gap-10 lg:gap-20 justify-between'>
+      <div className='flex gap-4 items-center flex-shrink-0'>
+        <button></button>
+        <a href='/'>
+          <img src={logo} alt='logo' className='h-6' />
+        </a>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
