d386f997e690311b8543b4c080b3ee105d40d069
Annie Huang <annie.hn@gmail.com>
2023-10-26 17:11:29 +1100

    Add buttonStyles through cva (class-variance-authority)

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,28 @@
-export const Button = () => {
-  return <button />;
+import { cva } from 'class-variance-authority';
+
+// secondary-hover is defined in tailwind.config.js, surprised tailwind can just add 'bg-' in front.
+const buttonStyles = cva(['transition-colors'], {
+  variants: {
+    variant: {
+      default: ['bg-secondary', 'hover:bg-secondary-hover'],
+      ghost: ['hover:bg-gray-100'],
+    },
+    size: {
+      default: ['rounded', 'p-2'],
+      // icon button styling:
+      icon: [
+        'rounded-full',
+        'w-10',
+        'h-10',
+        'flex',
+        'items-center',
+        'justify-center',
+        'p-2.5',
+      ],
+    },
+  },
+});
+
+export const Button = ({ variant, size }) => {
+  return <button className={buttonStyles({ variant, size })} />;
 };
