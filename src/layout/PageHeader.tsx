34fe5f64d90ef811902f56f13c974edba2db1749
Annie Huang <annie.hn@gmail.com>
2023-10-28 14:08:39 +1100

    Add the right side 3 buttons on the Header bar

--- src/layout/PageHeader.tsx
+++ src/layout/PageHeader.tsx
@@ -1,10 +1,10 @@
 import logo from '../assets/Logo.png';
-import { Menu } from 'lucide-react';
+import { Bell, Menu, Upload, User } from 'lucide-react';
 import { Button } from '../components/Button.tsx';
 
 export const PageHeader = () => {
   return (
-    <div className='flex gap-10 lg:gap-20 justify-between'>
+    <div className='flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4'>
       <div className='flex gap-4 items-center flex-shrink-0'>
         <Button variant='ghost' size='icon'>
           <Menu />
@@ -14,7 +14,17 @@ export const PageHeader = () => {
         </a>
       </div>
       <div></div>
-      <div></div>
+      <div className='flex flex-shrink-0 md:gap-2'>
+        <Button variant='ghost' size='icon'>
+          <Upload />
+        </Button>
+        <Button variant='ghost' size='icon'>
+          <Bell />
+        </Button>
+        <Button variant='ghost' size='icon'>
+          <User />
+        </Button>
+      </div>
     </div>
   );
 };
