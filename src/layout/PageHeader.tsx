7c24d46bbaffc37604c1862c9e7852c7020c1cdd
Annie Huang <annie.hn@gmail.com>
2023-10-28 15:06:15 +1100

    Search bar only show in tablet or above, hidden in mobile

--- src/layout/PageHeader.tsx
+++ src/layout/PageHeader.tsx
@@ -13,7 +13,7 @@ export const PageHeader = () => {
           <img src={logo} alt='logo' className='h-6' />
         </a>
       </div>
-      <form className='flex gap-4 flex-grow justify-center'>
+      <form className='md:flex hidden gap-4 flex-grow justify-center'>
         <div className='flex flex-grow max-w-[600px]'>
           {/* The input will shrink as the screen gets small, and grow as the screen grows to a max 600px width */}
           <input
