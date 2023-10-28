7b86b53432fd3f0bcc44c91c9167e5668edacd72
Annie Huang <annie.hn@gmail.com>
2023-10-28 16:57:05 +1100

    Create two columns for the <main> and the right side will grow as screen width increase, and have scrollbar vertically as well.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ export default function App() {
   return (
     <div className='max-h-screen flex flex-col'>
       <PageHeader />
+      <div className='grid grid-col-[auto, 1fr] flex-grow-1 overflow-auto'>
+        <div>Sidebar</div>
+        <div></div>
+      </div>
     </div>
   );
 }
