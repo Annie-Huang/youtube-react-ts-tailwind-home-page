dd93ea568cc77b4fe0e558049126c8ca3eff2dbf
Annie Huang <annie.hn@gmail.com>
2023-10-28 14:35:38 +1100

    Add padding the margin. and when it is on focus, use border not outline

--- src/layout/PageHeader.tsx
+++ src/layout/PageHeader.tsx
@@ -18,7 +18,7 @@ export const PageHeader = () => {
           <input
             type='search'
             placeholder='Search'
-            className='rounded-l-full border border-secondary-border shadow-inner shadow-secondary'
+            className='rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none'
           />
           <Button>
             <Search />
