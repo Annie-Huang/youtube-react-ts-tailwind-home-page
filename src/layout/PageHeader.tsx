31f242f40594cbd5d64f2d59ff48c509100907ff
Annie Huang <annie.hn@gmail.com>
2023-10-28 15:20:47 +1100

    When showFullWidthSearch is true, hide the left side group and right side group in the <Header>

--- src/layout/PageHeader.tsx
+++ src/layout/PageHeader.tsx
@@ -8,7 +8,11 @@ export const PageHeader = () => {
 
   return (
     <div className='flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4'>
-      <div className='flex gap-4 items-center flex-shrink-0'>
+      <div
+        className={`gap-4 items-center flex-shrink-0 ${
+          showFullWidthSearch ? 'hidden' : 'flex'
+        }`}
+      >
         <Button variant='ghost' size='icon'>
           <Menu />
         </Button>
@@ -32,7 +36,11 @@ export const PageHeader = () => {
           <Mic />
         </Button>
       </form>
-      <div className='flex flex-shrink-0 md:gap-2'>
+      <div
+        className={`flex flex-shrink-0 md:gap-2 ${
+          showFullWidthSearch ? 'hidden' : 'flex'
+        }`}
+      >
         <Button
           onClick={() => setShowFullWidthSearch(true)}
           variant='ghost'
