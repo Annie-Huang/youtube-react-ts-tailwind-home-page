07e8de872bb245222454c8bccc29170928365e96
Annie Huang <annie.hn@gmail.com>
2023-10-28 17:40:59 +1100

    Add a dark variant into the <Button>

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -4,7 +4,12 @@ export const CategoryPills = () => {
   return (
     <div className='overflow-x-hidden relative'>
       <div className='flex whitespace-nowrap gap-3 transition-transform w-[max-content]'>
-        <Button className='py-1 px-3 rounded-lg whitespace-nowrap'>All</Button>
+        <Button
+          variant='dark'
+          className='py-1 px-3 rounded-lg whitespace-nowrap'
+        >
+          All
+        </Button>
         <Button className='py-1 px-3 rounded-lg whitespace-nowrap'>
           JavaScript
         </Button>
