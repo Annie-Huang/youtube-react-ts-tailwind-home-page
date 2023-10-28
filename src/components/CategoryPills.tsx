af8996cea642a5ef09a2b9b8e5d4afc3124cb214
Annie Huang <annie.hn@gmail.com>
2023-10-28 17:31:55 +1100

    Create a bar and sub container to have all the buttons. Add one button.

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,3 +1,12 @@
+import { Button } from './Button';
+
 export const CategoryPills = () => {
-  return <div>feiwja</div>;
+  return (
+    <div className='overflow-x-hidden relative'>
+      {/* Don't need to add whitespace-nowrap because flex by default is nowrap already. */}
+      <div className='flex whitespace-nowrap gap-3 transition-transform w-[max-content]'>
+        <Button>All</Button>
+      </div>
+    </div>
+  );
 };
