6f9d5fb5fc6ad7afa752392c6eab38f7831946cd
Annie Huang <annie.hn@gmail.com>
2023-10-28 17:37:49 +1100

    Create 2 buttons

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -3,9 +3,11 @@ import { Button } from './Button';
 export const CategoryPills = () => {
   return (
     <div className='overflow-x-hidden relative'>
-      {/* Don't need to add whitespace-nowrap because flex by default is nowrap already. */}
       <div className='flex whitespace-nowrap gap-3 transition-transform w-[max-content]'>
-        <Button>All</Button>
+        <Button className='py-1 px-3 rounded-lg whitespace-nowrap'>All</Button>
+        <Button className='py-1 px-3 rounded-lg whitespace-nowrap'>
+          JavaScript
+        </Button>
       </div>
     </div>
   );
