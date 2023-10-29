036f22f97adbaca15c8b0841d04ee70a5954823d
Annie Huang <annie.hn@gmail.com>
2023-10-29 13:31:07 +1100

    Create two local state: isLeftVisible and isRightVisible and only show the <ChevronLeft> button if isLeftVisible is true

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,5 +1,6 @@
 import { Button } from './Button';
 import { ChevronLeft } from 'lucide-react';
+import { useState } from 'react';
 
 type CategoryPillProps = {
   categories: string[];
@@ -12,6 +13,9 @@ export const CategoryPills = ({
   selectedCategory,
   onSelect,
 }: CategoryPillProps) => {
+  const [isLeftVisible, setIsLeftVisible] = useState(false);
+  const [isRightVisible, setIsRightVisible] = useState(false);
+
   return (
     <div className='overflow-x-hidden relative'>
       <div className='flex whitespace-nowrap gap-3 transition-transform w-[max-content]'>
@@ -28,15 +32,17 @@ export const CategoryPills = ({
       </div>
 
       {/* From 50% (middle of <Button>) to 96px, add linear-gradient white-> transparent. */}
-      <div className='absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full'>
-        <Button
-          variant='ghost'
-          size='icon'
-          className='h-full aspect-square w-auto p-1.5'
-        >
-          <ChevronLeft />
-        </Button>
-      </div>
+      {isLeftVisible && (
+        <div className='absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full'>
+          <Button
+            variant='ghost'
+            size='icon'
+            className='h-full aspect-square w-auto p-1.5'
+          >
+            <ChevronLeft />
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
