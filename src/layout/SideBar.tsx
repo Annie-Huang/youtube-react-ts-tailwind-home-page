acc75c40b7a9cd417839695069df9929651156b6
Annie Huang <annie.hn@gmail.com>
2023-11-05 13:38:01 +1100

    Add <LargeSidebarSection> and <LargeSidebarItem> template for desktop sidebar

--- src/layout/SideBar.tsx
+++ src/layout/SideBar.tsx
@@ -16,7 +16,11 @@ export const SideBar = () => {
         />
         <SmallSidebarItem Icon={Library} title='Library' url='/library' />
       </aside>
-      <aside className='w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 flex'></aside>
+      <aside className='w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 flex'>
+        <LargeSidebarSection>
+          <LargeSidebarItem isActive Icon={Home} title='Home' url='/' />
+        </LargeSidebarSection>
+      </aside>
     </>
   );
 };
