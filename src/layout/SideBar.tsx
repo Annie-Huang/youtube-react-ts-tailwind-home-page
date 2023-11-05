3ade044b2b9e676cb39160fa85879fba4884e599
Annie Huang <annie.hn@gmail.com>
2023-11-05 13:16:33 +1100

    Create the <SmallSidebarItem> element in SideBar.tsx

--- src/layout/SideBar.tsx
+++ src/layout/SideBar.tsx
@@ -4,7 +4,21 @@ import { ElementType } from 'react';
 export const SideBar = () => {
   return (
     <aside className='sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden'>
-      <SmallSidebarItem icon={Home} title='Home' url='/' />
+      <SmallSidebarItem Icon={Home} title='Home' url='/' />
     </aside>
   );
 };
+
+type SmallSidebarItemProps = {
+  Icon: ElementType;
+  title: string;
+  url: string;
+};
+function SmallSidebarItem({ Icon, title, url }: SmallSidebarItemProps) {
+  return (
+    <a href={url} className=''>
+      <Icon className='w-6 h-6' />
+      <span className='text-sm'>{title}</span>
+    </a>
+  );
+}
