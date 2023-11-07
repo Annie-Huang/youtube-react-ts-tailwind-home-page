3a4a14d40f676f93e6a284cb5ae2f393ef5c5a45
Annie Huang <annie.hn@gmail.com>
2023-11-07 13:32:58 +1100

    Trigger toggle when Header Menu icon is clicked.

--- src/layout/PageHeader.tsx
+++ src/layout/PageHeader.tsx
@@ -2,9 +2,11 @@ import logo from '../assets/Logo.png';
 import { ArrowLeft, Bell, Menu, Mic, Search, Upload, User } from 'lucide-react';
 import { Button } from '../components/Button.tsx';
 import { useState } from 'react';
+import { useSidebarContext } from '../contexts/SidebarContext.tsx';
 
 export const PageHeader = () => {
   const [showFullWidthSearch, setShowFullWidthSearch] = useState(false);
+  const { toggle } = useSidebarContext();
 
   return (
     <div className='flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4'>
@@ -13,7 +15,7 @@ export const PageHeader = () => {
           showFullWidthSearch ? 'hidden' : 'flex'
         }`}
       >
-        <Button variant='ghost' size='icon'>
+        <Button onClick={toggle} variant='ghost' size='icon'>
           <Menu />
         </Button>
         <a href='/'>
